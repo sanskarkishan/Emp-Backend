9245e8a6427c9c10781d3df4275049edb6b6d6fe
sanskarkishan <sanskarkishan07@gmail.com>
2025-09-28 22:12:55 +0530

    Solve CROS issue

--- src/main/java/com/springProject/employee_poject/EmpController.java
+++ src/main/java/com/springProject/employee_poject/EmpController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://emp-database-system.netlify.app")
 public class EmpController {
     
     //EmployeeService employeeService = new EmployeeServiceImpl();
